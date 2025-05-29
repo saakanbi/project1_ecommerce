07ffdd506fcded636e66f9eafa0ea2a3d0ff2b09
saakanbi <woakanbi@gmail.com>
2025-05-28 22:46:41 -0600

    hhhhh

--- src/main/java/com/cyat/ecommerce/Main.java
+++ src/main/java/com/cyat/ecommerce/Main.java
@@ -8,8 +8,4 @@ public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 943ee0b81e73be5ba97817c57c11e5f82a79519b
